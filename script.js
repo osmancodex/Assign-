6559e969767c93da10828f4f1e009551e6a6fa97
Osman Codex <120652187+osmancodex@users.noreply.github.com>
2024-03-17 18:32:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+document.getElementById("alertName").onclick = function() {
+    alert("Osman Bin Saeed");
+}
+
+
+document.getElementById("alertNumber").onclick = function() {
+    alert("03043330988");
+}
+
+
+document.getElementById("showVariableName").onclick = function() {
+    document.getElementById("var1").style.visibility = "visible";
+    document.getElementById("var2").style.visibility = "visible";
+}
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("var1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("var2").style.visibility = "hidden";
+}
+
+
+// camelcase
+
+document.getElementById("camelCase").onclick = function() {
+    document.getElementById("camelCase1").style.visibility = "visible";
+    document.getElementById("camelCase2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("camelCase1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("camelCase2").style.visibility = "hidden";
+}
+
+
+
+// Sum of Number
+
+document.getElementById("sum").onclick = function() {
+    document.getElementById("sum1").style.visibility = "visible";
+    document.getElementById("sum2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("sum1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("sum2").style.visibility = "hidden";
+}
+
+
+// Subtract
+
+document.getElementById("sub").onclick = function() {
+    document.getElementById("sub1").style.visibility = "visible";
+    document.getElementById("sub2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("sub1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("sub2").style.visibility = "hidden";
+}
+
+
+// Multiplay
+
+document.getElementById("multi").onclick = function() {
+    document.getElementById("multi1").style.visibility = "visible";
+    document.getElementById("multi2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("multi1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("multi2").style.visibility = "hidden";
+}
+
+// Divide
+
+document.getElementById("divide").onclick = function() {
+    document.getElementById("divide1").style.visibility = "visible";
+    document.getElementById("divide2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("divide1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("divide2").style.visibility = "hidden";
+}
+
+
+// Calculate
+
+document.getElementById("calculate").onclick = function() {
+    document.getElementById("calculate1").style.visibility = "visible";
+    document.getElementById("calculate2").style.visibility = "visible";
+}
+
+
+document.getElementById("clear1").onclick = function() {
+    document.getElementById("calculate1").style.visibility = "hidden";
+}
+document.getElementById("clear2").onclick = function() {
+    document.getElementById("calculate2").style.visibility = "hidden";
+}
+
+
+
